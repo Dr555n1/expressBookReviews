b3b76bc60661b1d390408d5f37f37ca803110c22
Vladimir <grinfeldsv75@yahoo.com>
2025-01-11 15:16:57 -0500

    updating final project end

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -42,7 +42,19 @@ public_users.post("/register", (req,res) => {
 public_users.get('/',function (req, res) {
   //Write your code here
   //return res.status(300).json({message: "Yet to be implemented"});
-  res.send(JSON.stringify({books}, null, 4));
+  //res.send(JSON.stringify({books}, null, 4));
+  let getBookPromise = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        resolve(books);
+    },1000)})
+
+
+  getBookPromise.then((books) => { 
+    res.send(JSON.stringify({books}, null, 4));
+  }) 
+  .catch((error) => { 
+    res.status(500).json({error: "Error: the operation failed!"});
+  });
 });
 
 // Get book details based on ISBN
@@ -50,7 +62,27 @@ public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   //return res.status(300).json({message: "Yet to be implemented"});
   let book_isbn = req.params.isbn;
-  res.send(JSON.stringify(books[book_isbn], null, 4));
+  //res.send(JSON.stringify(books[book_isbn], null, 4));
+  let getBookPromise = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        let isbn_book = books[book_isbn];
+        if (isbn_book)
+        {
+            resolve(isbn_book);
+        }
+        else
+        {
+            reject(new Error("ISBN not found"))
+        }
+    },1000)})
+
+
+  getBookPromise.then((isbn_book) => { 
+    res.send(JSON.stringify({isbn_book}, null, 4));
+  }) 
+  .catch((error) => { 
+    res.status(400).json({"error": error+", Error: the operation failed!"});
+  });
  });
   
 // Get book details based on author
@@ -58,8 +90,32 @@ public_users.get('/author/:author',function (req, res) {
   //Write your code here
   //return res.status(300).json({message: "Yet to be implemented"});
   const book_author = req.params.author;
-  let filtered_books = Object.values(books).filter((book) => book.author === book_author);
-  res.send(filtered_books);
+  //let filtered_books = Object.values(books).filter((book) => book.author === book_author);
+  //res.send(filtered_books);
+  let getAuthorPromise = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        let filtered_books = Object.values(books).filter((book) => book.author === book_author);
+        if (filtered_books.length>0)
+        {
+            resolve(filtered_books);
+        }
+        else
+        {
+            reject(new Error("Author not found"))
+        }
+    },1000)})
+
+
+    getAuthorPromise.then((filtered_books) => { 
+    res.send(JSON.stringify({filtered_books}, null, 4));
+  }) 
+  .catch((error) => { 
+    //console.log(error);
+    res.status(400).json({"error": error+", Error: the operation failed!"});
+  });
+
+
+
 });
 
 // Get all books based on title
@@ -67,8 +123,30 @@ public_users.get('/title/:title',function (req, res) {
   //Write your code here
   //return res.status(300).json({message: "Yet to be implemented"});
   const book_title = req.params.title;
-  let filtered_books = Object.values(books).filter((book) => book.title === book_title);
-  res.send(filtered_books);
+  //let filtered_books = Object.values(books).filter((book) => book.title === book_title);
+  //res.send(filtered_books);
+  let getTitlePromise = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        let filtered_books = Object.values(books).filter((book) => book.title === book_title);
+        if (filtered_books.length>0)
+        {
+            resolve(filtered_books);
+        }
+        else
+        {
+            reject(new Error("Title not found"))
+        }
+    },1000)})
+
+
+    getTitlePromise.then((filtered_books) => { 
+    res.send(filtered_books);
+  }) 
+  .catch((error) => { 
+    //console.log(error);
+    res.status(400).json({"error": error+", Error: the operation failed!"});
+  });
+
 });
 
 //  Get book review
